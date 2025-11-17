2dfeb9c21addf2f24767542eb43793d7ec410716
jhonatancruzmail <jhonatancruzmail@gmail.com>
2025-11-16 23:12:43 -0500

    fix: better month formatting in formatLocalDateTime function
    - Updated the month assignment in the formatLocalDateTime function to directly use the month index instead of the month name, ensuring accurate date formatting.

--- src/shared/utils/formatUtils.ts
+++ src/shared/utils/formatUtils.ts
@@ -26,7 +26,7 @@ export function formatLocalDateTime(date: Date): string {
     ];
 
     const day = date.getDate().toString().padStart(2, "0");
-    const month = months[date.getMonth()];
+    const month = date.getMonth().toString().padStart(2, "0");
     const year = date.getFullYear();
     const hours = date.getHours().toString().padStart(2, "0");
     const minutes = date.getMinutes().toString().padStart(2, "0");
